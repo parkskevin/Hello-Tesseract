b08322a97e932377c3976371d0966531e3532954
kevin <parkske@oregonstate.edu>
2016-04-22 16:21:52 -0700

    Adds tess-two submodule. Adds tess-two into app project. Implements a very basic camera save to file.

--- app/src/main/java/com/kparks/hello_tesseract/MainActivity.java
+++ app/src/main/java/com/kparks/hello_tesseract/MainActivity.java
@@ -1,15 +1,36 @@
 package com.kparks.hello_tesseract;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int TAKE_PHOTO_REQUEST = 0;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    protected Uri mMediaUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +38,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //TODO: anything actually to do with TessBaseAPI, this serves as a hint it's there
+        TessBaseAPI tessBaseAPI = new TessBaseAPI();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Take a photo using system camera
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Find a location to store the image
+                mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                if(mMediaUri == null) {
+                    //display an error
+                    Toast.makeText(MainActivity.this, "Cannot save images", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    //Pass along the URI to use for saving
+                    takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                    //Start camera intent
+                    startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                }
             }
         });
     }
@@ -48,5 +83,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Uri getOutputMediaFileUri(int mediaType) {
+        //To be safe checkk SDCard (external) storage
+        //using Environment.getExternalStorageState() before doing this
+        if(isExternalStorageAvailable()) {
+            //get the Uri
+            //get the external storage directory
+            String appName = MainActivity.this.getString(R.string.app_name);
+            File mediaStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    appName);
+            //create our own sub dir
+            Log.d(TAG, "Path to dir: " + mediaStorageDir.toString());
+            if(! mediaStorageDir.exists()) {
+                if(!mediaStorageDir.mkdirs()) {
+                    Log.e(TAG, "Failed to create directory");
+                    return null;
+                }
+            }
+            //create filename
+            //create file
+            File mediaFile;
+            Date now = new Date();
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+            String path = mediaStorageDir.getPath() + File.separator;
+            mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+
+            Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+            //return the file's uri
+            return Uri.fromFile(mediaFile);
+        }
+        else {
+            return null;
+        }
+    }
+
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        if(state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //Let system know we added something
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(mMediaUri);
+            sendBroadcast(mediaScanIntent);
+        } else {
+            Toast.makeText(this, "Camera intent failed", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 }
